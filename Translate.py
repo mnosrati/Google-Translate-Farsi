0f8885fd7ad909e3e1563f47d15ff9d55e39d7ce
mnosrati <37707329+mnosrati@users.noreply.github.com>
2019-06-10 20:26:20 -0500

    Add files via upload

--- Translate.py
+++ Translate.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
-
+import numpy
+import string
+import time
 
 class Translate:
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome("./driver/chromedriver", chrome_options=options)
-    driver.set_page_load_timeout(-1)
+
 
     Farsi = []
     Farsi_POS = []
@@ -14,7 +13,19 @@ class Translate:
     English_POS = []
     Example = []
     Synonym = ""
-
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver = webdriver.Chrome("./driver/chromedriver", chrome_options=options)
+        driver.set_page_load_timeout(-1)
+    except Exception as e:
+        Farsi = ["(an error occured)"]
+        Farsi_POS = ["(an error occured)"]
+        English_DEF = ["(an error occured)"]
+        English_EXM = ["(an error occured)"]
+        English_POS = ["(an error occured)"]
+        Example = ["(an error occured)"]
+        Synonym = "(an error occured)"
 
     page="https://translate.google.com/#view=home&op=translate&sl=en&tl=fa&text="
     divs="gt-cd-c"
@@ -29,79 +40,109 @@ class Translate:
         self.Example = []
         self.Synonym = ""
 
-
-        self.driver.get(self.page + word)
-
-
-        contents = self.driver.find_elements_by_class_name(self.divs)
-        fa=contents[0].text.splitlines()
-        en=contents[1].get_attribute('innerHTML')
-        ex=contents[2].get_attribute('innerHTML')
-        sy=contents[3].text
-
+        try:
+            self.driver.get(self.page + word)
+            contents = self.driver.find_elements_by_class_name(self.divs)
+            fa=contents[0].text.splitlines()
+            en=contents[1].get_attribute('innerHTML')
+            ex=contents[2].get_attribute('innerHTML')
+            sy=contents[3].text
+        except Exception as e:
+            Farsi = ["(an error occured)"]
+            Farsi_POS = ["(an error occured)"]
+            English_DEF = ["(an error occured)"]
+            English_EXM = ["(an error occured)"]
+            English_POS = ["(an error occured)"]
+            Example = ["(an error occured)"]
+            Synonym = "(an error occured)"
 
         #Get Farsi window
         #------------------------------------------------------------
-        translation=""
-        newFlag=0
-        for i in range(0,len(fa)):
-            tmpStr=fa[i]
+        try:
+            translation=""
+            newFlag=0
+            for i in range(0,len(fa)):
+                tmpStr=fa[i]
 
-            if(tmpStr[0].isupper() and tmpStr != 'Frequency'):
-                self.Farsi_POS.append(tmpStr)
-                newFlag=1
-            else:
-                if(self.isEnglish(tmpStr)==False):
-                    if(newFlag==1):
-                        if(translation!=""):
-                            self.Farsi.append(translation)
-                        translation = tmpStr
-                        newFlag=0
-                    else:
-                        translation = translation + "؛ " + tmpStr
-        if(translation!=""):
-            self.Farsi.append(translation)
-
-
+                if(tmpStr[0].isupper() and tmpStr != 'Frequency'):
+                    self.Farsi_POS.append(tmpStr)
+                    newFlag=1
+                else:
+                    if(self.isEnglish(tmpStr)==False):
+                        if(newFlag==1):
+                            if(translation!=""):
+                                self.Farsi.append(translation)
+                            translation = tmpStr
+                            newFlag=0
+                        else:
+                            translation = translation + "؛ " + tmpStr
+            if(translation!=""):
+                self.Farsi.append(translation)
+        except Exception as e:
+            self.Farsi= ["(an error occured)"]
+            self.Farsi_POS= ["(an error occured)"]
 
         #Get English window
         #------------------------------------------------------------
-        self.driver.get("data:text/html;charset=utf-8,{en}".format(en=en))
-        poses=self.driver.find_elements_by_class_name("gt-cd-pos")
-        for pos in poses:
-            self.English_POS.append(pos.text)
+        try:
+            self.driver.get("data:text/html;charset=utf-8,{en}".format(en=en))
+            poses=self.driver.find_elements_by_class_name("gt-cd-pos")
+            for pos in poses:
+                self.English_POS.append(pos.text)
 
-        def_list=self.driver.find_elements_by_class_name("gt-def-list")
+            def_list=self.driver.find_elements_by_class_name("gt-def-list")
 
-        for i in range(0,len(def_list)):
-            def_lines=def_list[i].text.splitlines()
-            for j in range(0,len(def_lines)):
-                if(def_lines[j].isdigit()):
-                    self.English_DEF.append(def_lines[j+1])
-                    try:
-                        self.English_EXM.append(def_lines[j+2])
-                    except IndexError:
-                        self.English_EXM.append("(no example)")
-
+            for i in range(0,len(def_list)):
+                def_lines=def_list[i].text.splitlines()
+                for j in range(0,len(def_lines)):
+                    if(def_lines[j].isdigit()):
+                        self.English_DEF.append(def_lines[j+1])
+                        try:
+                            self.English_EXM.append(def_lines[j+2])
+                        except IndexError:
+                            self.English_EXM.append("(no example)")
+        except Exception as e:
+            self.English_DEF = ["(an error occured)"]
+            self.English_EXM = ["(an error occured)"]
+            self.English_POS = ["(an error occured)"]
 
 
 
         #Get English examples
         #------------------------------------------------------------
-        self.driver.get("data:text/html;charset=utf-8,{ex}".format(ex=ex))
-        exmps=self.driver.find_elements_by_class_name("gt-ex-top")
+        try:
+            self.driver.get("data:text/html;charset=utf-8,{ex}".format(ex=ex))
+            exmps=self.driver.find_elements_by_class_name("gt-ex-top")
 
-        for exmp in exmps:
-            self.Example.append(exmp.text)
-
+            for exmp in exmps:
+                self.Example.append(exmp.text)
+        except Exception as e:
+            self.Example = ["(an error occured)"]
 
 
 
         #Get Synonyms
         #------------------------------------------------------------
-        self.Synonym=sy
+        try:
+            self.Synonym=sy
+        except Exception as e:
+            self.Synonym = "(an error occured)"
 
 
+        if (len(self.Farsi)==0):
+            self.Farsi=[""]
+        if (len(self.Farsi_POS)==0):
+            self.Farsi_POS=[""]
+        if (len(self.English_POS)==0):
+            self.English_POS=[""]
+        if (len(self.English_EXM)==0):
+            self.English_EXM=[""]
+        if (len(self.English_DEF)==0):
+            self.English_DEF=[""]
+        if (len(self.Example)==0):
+            self.Example=[""]
+        if (len(self.Synonym)==0):
+            self.Synonym=""
 
 
     def isEnglish(self,s):
